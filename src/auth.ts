434d471fb93c7251206af3dd00da459772097419
Orji Michael Chukwuebuka <orjimichael@4886@gmail.com>
2025-02-09 15:53:11 +0100

    Add auth routes

--- src/auth.ts
+++ src/auth.ts
@@ -5,7 +5,7 @@ import { db } from '@/db';
 
 const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
 const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
-const AUTH_SECRET = process.env.AUTH_SECRET;
+// const AUTH_SECRET = process.env.AUTH_SECRET;
 
 if (!GITHUB_CLIENT_ID || GITHUB_CLIENT_SECRET)
   throw new Error('Missing Github auth credentials');
