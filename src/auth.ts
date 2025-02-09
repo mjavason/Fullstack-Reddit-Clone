72effba3b532254b7868adcd2cec0d3b9cb58d06
Orji Michael Chukwuebuka <orjimichael@4886@gmail.com>
2025-02-09 16:16:05 +0100

    Add sign in and sign out functionality

--- src/auth.ts
+++ src/auth.ts
@@ -7,7 +7,7 @@ const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
 const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
 // const AUTH_SECRET = process.env.AUTH_SECRET;
 
-if (!GITHUB_CLIENT_ID || GITHUB_CLIENT_SECRET)
+if (!GITHUB_CLIENT_ID || !GITHUB_CLIENT_SECRET)
   throw new Error('Missing Github auth credentials');
 
 export const {
