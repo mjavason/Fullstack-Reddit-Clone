933069e0522d689563d173d69cc6717eebe3de49
Orji Michael Chukwuebuka <orjimichael@4886@gmail.com>
2025-02-09 16:22:32 +0100

    Add session provider to layout.tsx

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,11 +2,16 @@
 
 import { HeroUIProvider } from '@heroui/react';
 import React from 'react';
+import { SessionProvider } from 'next-auth/react';
 
 interface ProvidersProps {
   children: React.ReactNode;
 }
 
 export default function Providers({ children }: ProvidersProps) {
-  return <HeroUIProvider>{children}</HeroUIProvider>;
+  return (
+    <SessionProvider>
+      <HeroUIProvider>{children}</HeroUIProvider>
+    </SessionProvider>
+  );
 }
