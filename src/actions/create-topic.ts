0593ecacd6c2d55a503148047360d28ce8ecf32e
Orji Michael Chukwuebuka <orjimichael@4886@gmail.com>
2025-02-10 16:29:59 +0100

    Add zod validation schema for createTopic

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,3 +1,21 @@
 'use server';
 
-export async function createTopic() {}
+import * as actions from '@/actions';
+import { z } from 'zod';
+
+const createTopicSchema = z.object({
+  name: z
+    .string()
+    .min(3)
+    .regex(/^[a-z]/, {
+      message: 'Must be lowercase letters or dashes without',
+    }),
+  description: z.string().min(10),
+});
+
+export async function createTopic(formData: FormData) {
+  const name = formData.get('name');
+  const description = formData.get('description');
+
+  console.log(name, description);
+}
--- src/components/topics/topic-create-form.tsx
+++ src/components/topics/topic-create-form.tsx
@@ -1,3 +1,4 @@
+import * as actions from '@/actions';
 import {
   Input,
   Button,
@@ -15,18 +16,20 @@ export default function TopicCreateForm() {
         <Button color='primary'>Create a Topic</Button>
       </PopoverTrigger>
       <PopoverContent>
-        <form action=''>
+        <form action={actions.createTopic}>
           <div className='flex flex-col gap-4 p-4 w-80'>
             <h3 className='text-lg'>Create a topic</h3>
             <Input
+              name='name'
               label='Name'
               labelPlacement='outside'
               placeholder='Name'
             ></Input>
             <Textarea
-              label='Name'
+              name='description'
+              label='Description'
               labelPlacement='outside'
-              placeholder='Name'
+              placeholder='Description'
             ></Textarea>
             <Button type='submit'>Submit</Button>
           </div>
