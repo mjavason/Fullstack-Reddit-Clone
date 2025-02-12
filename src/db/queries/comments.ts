6bce9c32570507a46cd035a4a7a1c45686d42237
Orji Michael Chukwuebuka <orjimichael@4886@gmail.com>
2025-02-12 09:32:56 +0100

    Update migration scripts

--- src/db/queries/comments.ts
+++ src/db/queries/comments.ts
@@ -6,15 +6,13 @@ export type CommentWithAuthor = Comment & {
   user: { name: string | null; image: string | null };
 };
 
-export const fetchCommentsByPostId = cache((
-  postId: string
-): Promise<CommentWithAuthor[]> => {
-  console.log('Making a query');
-
-  return db.comment.findMany({
-    where: { postId },
-    include: {
-      user: { select: { name: true, image: true } },
-    },
-  });
-})
+export const fetchCommentsByPostId = cache(
+  (postId: string): Promise<CommentWithAuthor[]> => {
+    return db.comment.findMany({
+      where: { postId },
+      include: {
+        user: { select: { name: true, image: true } },
+      },
+    });
+  }
+);
