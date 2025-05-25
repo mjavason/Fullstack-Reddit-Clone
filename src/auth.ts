634f0f5ef84e1f95c423e13da322d59d0d208b9a
Michael Orji <Orjimichael4886@gmail.com>
2025-05-26 00:48:22 +0100

    Add build workflow (#1)
    * Fix workflow name formatting in build.yml
    
    * Refactor GitHub authentication credentials handling in auth.ts
    
    ---------
    
    Co-authored-by: Orji Michael Chukwuebuka <orjimichael@4886@gmail.com>

--- src/auth.ts
+++ src/auth.ts
@@ -3,11 +3,8 @@ import Github from 'next-auth/providers/github';
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import { db } from '@/db';
 
-const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
-const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
-
-if (!GITHUB_CLIENT_ID || !GITHUB_CLIENT_SECRET)
-  throw new Error('Missing Github auth credentials');
+const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID ?? 'client_id';
+const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET ?? 'secret';
 
 export const {
   handlers: { GET, POST },
